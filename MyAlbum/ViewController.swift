b1ea37b3d3664cafafff1224208b148bc5413cea
judy5050 <68409061+judy5050@users.noreply.github.com>
2021-01-31 23:17:33 +0900

    refresh벼튼 누를 경우 가격 정보가 변경되도록 추가

--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue=0
+    @IBOutlet weak var priceLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        priceLable.text="₩ \(currentValue)"
         // Do any additional setup after loading the view.
     }
 
     
     @IBAction func hello(_ sender: Any) {
-        let alert=UIAlertController(title:"Hello",message: "MY First App!!!",preferredStyle: .alert)
+        let message="가격은 ₩\(currentValue)입니다."
+        
+        let alert=UIAlertController(title:"Hello",message: message,preferredStyle: .alert)
         
         let action=UIAlertAction(title: "OK", style:.default, handler: nil)
 
         alert.addAction(action)
         present(alert,animated: true,completion: nil)
         
-        
+       let randomPrice = arc4random_uniform(10000)+1
+        currentValue = Int(randomPrice)
+        priceLable.text="₩ \(currentValue)"
     }
     
     
